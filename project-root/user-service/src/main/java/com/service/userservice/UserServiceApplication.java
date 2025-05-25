5d43ce6d684bd23a5881a8bdf5c0ee68f820a229
Viswas Reddy <101576555+ctrl-V-R@users.noreply.github.com>
2025-05-25 05:48:39 +0000

    barebones

---
+++ project-root/user-service/src/main/java/com/service/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient  // Enables Eureka client
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
